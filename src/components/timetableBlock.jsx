1b1e5e0d9eb734163a042fa8c1a52dfc64040332
HRCK0 <proka2000@gmail.com>
2019-12-01 13:51:34 -0500

    Added TimetableBlock class

---
+++ src/components/timetableBlock.jsx
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+
+export default class TimetableBlock{
+    state = {
+        empty: undefined
+    }
+
+    constructor(empty){
+        this.state.empty = empty;
+        this._courseInfo = {
+            courseType: {
+                lab: false,
+                lecture: false,
+                dgd: false,
+                tutorial: false
+            },
+            startTime: null,
+            endTime: null,
+            profTeaching: null,
+            
+            // endTime: startTime => {
+            //     let hours = parseInt(startTime.substring(0, 2));
+            //     let minutes = parseInt(startTime.substring(3, 5));
+
+            //     hours += 1;
+            //     minutes += 20;
+                
+            //     while (minutes > 60){
+            //         minutes -= 60;
+            //         hours += 1;
+            //     }
+
+            //     return (hours < 10? '0': '') + hours + ':' + minutes;
+            // }
+
+        }
+    }
+    
+    render(){
+
+    }
+}
