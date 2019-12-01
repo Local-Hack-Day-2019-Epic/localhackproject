739cf46a404472be49b94d729641a9ef78ea0b80
Chris Vettese <cvett018@uottawa.ca>
2019-12-01 14:45:55 -0500

    add schedule button

--- src/App.js
+++ src/App.js
@@ -51,6 +51,9 @@ class App extends React.Component {
                 courseCodeValue: ""
             });
         };
+        const _viewSchedules = () => {
+            
+        }
         return (
             <div className="App">
                 <header className="App-header">
@@ -58,27 +61,27 @@ class App extends React.Component {
                     <Delay wait={delayAmount}>
                         <Dropdown options={options} onChange={_onSelect} value={defaultOption}
                                   placeholder="Select an option"/>
-                        <li>
-                            <label>
-                                Enter Course Code:
-                                <input type="text" value={this.state.courseCodeValue} name="course_code_input"
-                                       onChange={_handleChange}/>
-                            </label>
-                            <input type="submit" value="Add" onClick={_onSubmit}/>
-                            <React.Fragment>
-                                <ul className="list-group">
-                                    {
-                                        this.state.courses.map(course => (
-                                            <li
-                                                key={course}
-                                                className="list-group-item list-group-item-primary"
-                                            >
-                                                {course}
-                                            </li>
-                                        ))}
-                                </ul>
-                            </React.Fragment>
-                        </li>
+
+                        <label>
+                            Enter Course Code:
+                            <input type="text" value={this.state.courseCodeValue} name="course_code_input"
+                                   onChange={_handleChange}/>
+                        </label>
+                        <input type="submit" value="Add" onClick={_onSubmit}/>
+                        <React.Fragment>
+                            <ul className="list-group">
+                                {
+                                    this.state.courses.map(course => (
+                                        <li
+                                            key={course}
+                                            className="list-group-item list-group-item-primary"
+                                        >
+                                            {course}
+                                        </li>
+                                    ))}
+                            </ul>
+                        </React.Fragment>
+                        <input type="submit" value="View Schedules" onClick={_viewSchedules}/>
                     </Delay>
                 </header>
             </div>
