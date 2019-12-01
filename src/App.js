ad1a20a45ee7f3492d8d03656410d3f45b693327
Chris Vettese <cvett018@uottawa.ca>
2019-12-01 11:56:00 -0500

    course array updates

--- src/App.js
+++ src/App.js
@@ -6,27 +6,59 @@ import 'react-dropdown/style.css'
 const options = [
   'Select Semester', 'Fall 2019', 'Winter 2020'
 ]
-const defaultOption = options[0]
+const defaultOption = options[0];
 
 function App() {
+  let courses = {
+    list_of_courses: ["test1", "test2", "test3"]
+  }
+  let courseValue = "";
+
   const _onSelect = () => {
 
   };
+  const _handleChange = (event) => {
+    courseValue = event.target.value;
+  }
 
+  const _onSubmit = () => {
+    courses.list_of_courses.push(courseValue);
+    courses.list_of_courses.map(course => (
+        <li
+            key={course}
+            className="list-group-item list-group-item-primary"
+        >
+          {course}
+        </li>
+    ))
+  };
   return (
     <div className="App">
       <header className="App-header">
         <Dropdown options={options} onChange={_onSelect} value={defaultOption} placeholder="Select an option" />
-        <form>
+        <form onSubmit={_onSubmit}>
           <label>
             Enter Course Code:
-            <input type="text" name="course_code_input" />
+            <input type="text" name="course_code_input" onChange={_handleChange}/>
           </label>
-          <input type="submit" value="Add" />
+          <input type="submit" value="Add"/>
+          <React.Fragment>
+            <ul className="list-group">
+              {courses.list_of_courses.map(course => (
+                  <li
+                      key={course}
+                      className="list-group-item list-group-item-primary"
+                  >
+                    {course}
+                  </li>
+              ))}
+            </ul>
+          </React.Fragment>
         </form>
       </header>
     </div>
   );
+
 }
 
 export default App;
