d2e321aa3d7373b00669ce0d35555f9b906c57b3
Chris Vettese <cvett018@uottawa.ca>
2019-12-01 13:03:24 -0500

    Converted App.js to class format

--- src/App.js
+++ src/App.js
@@ -3,62 +3,68 @@ import './App.css';
 
 import Dropdown from 'react-dropdown'
 import 'react-dropdown/style.css'
+import ReactDOM from 'react-dom'
 const options = [
   'Select Semester', 'Fall 2019', 'Winter 2020'
 ]
 const defaultOption = options[0];
 
-function App() {
-  let courses = {
-    list_of_courses: ["test1", "test2", "test3"]
-  }
-  let courseValue = "";
-
-  const _onSelect = () => {
-
-  };
-  const _handleChange = (event) => {
-    courseValue = event.target.value;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  const _onSubmit = () => {
-    courses.list_of_courses.push(courseValue);
-    courses.list_of_courses.map(course => (
-        <li
-            key={course}
-            className="list-group-item list-group-item-primary"
-        >
-          {course}
-        </li>
-    ))
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Dropdown options={options} onChange={_onSelect} value={defaultOption} placeholder="Select an option" />
-        <form onSubmit={_onSubmit}>
-          <label>
-            Enter Course Code:
-            <input type="text" name="course_code_input" onChange={_handleChange}/>
-          </label>
-          <input type="submit" value="Add"/>
-          <React.Fragment>
-            <ul className="list-group">
-              {courses.list_of_courses.map(course => (
-                  <li
-                      key={course}
-                      className="list-group-item list-group-item-primary"
-                  >
-                    {course}
-                  </li>
-              ))}
-            </ul>
-          </React.Fragment>
-        </form>
-      </header>
-    </div>
-  );
+  render() {
+    let courses = {
+      list_of_courses: ["test1", "test2", "test3"]
+    }
+    let courseValue = "";
 
+    const _onSelect = () => {
+
+    };
+    const _handleChange = (event) => {
+      courseValue = event.target.value;
+    };
+
+    const _onSubmit = () => {
+      courses.list_of_courses.push(courseValue);
+      courses.list_of_courses.map(course => (
+          <li
+              key={course}
+              className="list-group-item list-group-item-primary"
+          >
+            {course}
+          </li>
+      ));
+    };
+    return (
+        <div className="App">
+          <header className="App-header">
+            <Dropdown options={options} onChange={_onSelect} value={defaultOption} placeholder="Select an option"/>
+            <form onSubmit={_onSubmit}>
+              <label>
+                Enter Course Code:
+                <input type="text" name="course_code_input" onChange={_handleChange}/>
+              </label>
+              <input type="submit" value="Add"/>
+              <React.Fragment>
+                <ul className="list-group">
+                  {courses.list_of_courses.map(course => (
+                      <li
+                          key={course}
+                          className="list-group-item list-group-item-primary"
+                      >
+                        {course}
+                      </li>
+                  ))}
+                </ul>
+              </React.Fragment>
+            </form>
+          </header>
+        </div>
+    );
+
+  }
 }
-
 export default App;
