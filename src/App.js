67bfa5f41e837558d2ce83a17e9a6114804a3249
Chris Vettese <cvett018@uottawa.ca>
2019-12-01 10:26:13 -0500

    Added dropdown menu

--- src/App.js
+++ src/App.js
@@ -4,31 +4,20 @@ import './App.css';
 
 import Dropdown from 'react-dropdown'
 import 'react-dropdown/style.css'
-const defaultOption = options[0]
 const options = [
   'one', 'two', 'three'
 ]
+const defaultOption = options[0]
 
 function App() {
+  const _onSelect = () => {
+
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
-
-
+        <Dropdown options={options} onChange={_onSelect} value={defaultOption} placeholder="Select an option" />
       </header>
     </div>
   );
